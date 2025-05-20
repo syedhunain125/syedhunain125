d97dc2726d48193e2128a48a70ce7d4821c29f87
syedhunain125 <syedhunainali27@gmail.com>
2025-05-20 04:24:50 -0700

    flask

--- app.py
+++ app.py
@@ -18,5 +18,6 @@ def skills():
 def contact():
     return render_template('contact.html')
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# Required for Vercel
+def handler(environ, start_response):
+    return app(environ, start_response)
